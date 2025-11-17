09e1a8381a9031e33e59ab4a48f85e20710ba018
SalahHassan202 <salahhassan66190@gmail.com>
2025-11-17 12:57:25 +0200

    added pimary button widget

---
+++ lib/core/widgets/primary_button_widget.dart
@@ -0,0 +1,48 @@
+import 'package:finance_ui_flutter/core/styling/app_colors.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class PrimayButtonWidget extends StatelessWidget {
+  final String? buttonText;
+  final Color? buttonColor;
+  final double? width;
+  final double? height;
+  final double? boarderRedius;
+  final Color? textColor;
+  final double? fontSize;
+  final void Function()? onPress;
+  const PrimayButtonWidget({
+    super.key,
+    this.buttonText,
+    this.buttonColor,
+    this.width,
+    this.height,
+    this.boarderRedius,
+    this.fontSize,
+    this.textColor,
+    this.onPress,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onPress,
+      style: ElevatedButton.styleFrom(
+        backgroundColor: buttonColor ?? AppColors.primaryColor,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(boarderRedius ?? 8.r),
+        ),
+        fixedSize: Size(width ?? 331.w, height ?? 56.h),
+      ),
+
+      child: Text(
+        buttonText ?? "",
+        style: TextStyle(
+          color: textColor ?? Colors.white,
+          fontWeight: FontWeight.bold,
+          fontSize: fontSize ?? 16.sp,
+        ),
+      ),
+    );
+  }
+}
