5959ba99e168a47e53109142b578e3fb7944c9f6
SalahHassan202 <salahhassan66190@gmail.com>
2025-11-18 18:26:50 +0200

    password changed

--- lib/features/passwod_changed/password_changed.dart
+++ lib/features/passwod_changed/password_changed.dart
@@ -1,3 +1,7 @@
+import 'package:finance_ui_flutter/core/styling/app_assets.dart';
+import 'package:finance_ui_flutter/core/styling/app_styles.dart';
+import 'package:finance_ui_flutter/core/widgets/primary_button_widget.dart';
+import 'package:finance_ui_flutter/core/widgets/spacing_widgets.dart';
 import 'package:flutter/material.dart';
 
 class PasswordChanged extends StatelessWidget {
@@ -5,6 +9,26 @@ class PasswordChanged extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SafeArea(
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 22),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            HeightSpace(248),
+            Image.asset(AppAssets.successmark),
+            HeightSpace(35),
+            Text("Password Changed!", style: AppStyles.primaryHeadLineStyle),
+            HeightSpace(8),
+            Text(
+              "Your password has been changed successfully.",
+              style: AppStyles.subtitlesStyles,
+            ),
+            HeightSpace(40),
+            PrimayButtonWidget(buttonText: "Back to Login"),
+          ],
+        ),
+      ),
+    );
   }
 }
