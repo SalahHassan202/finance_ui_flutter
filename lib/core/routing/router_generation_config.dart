aa79b7930c016113bfd66c06782fd5ea645e12b6
SalahHassan202 <salahhassan66190@gmail.com>
2025-11-21 21:26:50 +0200

    Configure MainScreen routes

--- lib/core/routing/router_generation_config.dart
+++ lib/core/routing/router_generation_config.dart
@@ -1,13 +1,14 @@
 import 'package:finance_ui_flutter/core/routing/app_route.dart';
 import 'package:finance_ui_flutter/features/all_cards/all_cards.dart';
 import 'package:finance_ui_flutter/features/auth/login_screen.dart';
-import 'package:finance_ui_flutter/features/create_new_password.dart/create_new_password.dart';
+import 'package:finance_ui_flutter/features/create_new_password/create_new_password.dart';
 import 'package:finance_ui_flutter/features/forget_password/forget_password.dart';
+import 'package:finance_ui_flutter/features/main_screen/main_screen.dart';
 import 'package:finance_ui_flutter/features/my_profile/my_profile.dart';
 import 'package:finance_ui_flutter/features/on_boarding_screen/on_boarding_screen.dart';
 import 'package:finance_ui_flutter/features/passwod_changed/password_changed.dart';
-import 'package:finance_ui_flutter/features/statistics/statistics_screen.dart';
 import 'package:finance_ui_flutter/features/verify_otp/verify_otp_screen.dart';
+import 'package:finance_ui_flutter/features/statistics/statistics_screen.dart';
 import 'package:go_router/go_router.dart';
 
 class RouterGenerationConfig {
@@ -33,6 +34,28 @@ class RouterGenerationConfig {
         path: AppRoutes.mainScreen,
         name: AppRoutes.mainScreen,
         builder: (context, state) => const MainScreen(),
+        routes: [
+          GoRoute(
+            path: 'home',
+            name: 'home',
+            builder: (context, state) => const MainScreen(),
+          ),
+          GoRoute(
+            path: 'statistics',
+            name: 'statistics',
+            builder: (context, state) => const StatisticsScreen(),
+          ),
+          GoRoute(
+            path: 'myCard',
+            name: AppRoutes.myCard,
+            builder: (context, state) => const MyCard(),
+          ),
+          GoRoute(
+            path: 'profile',
+            name: AppRoutes.profile,
+            builder: (context, state) => const MyProfile(),
+          ),
+        ],
       ),
       GoRoute(
         path: AppRoutes.forgetPassword,
@@ -58,6 +81,11 @@ class RouterGenerationConfig {
         path: AppRoutes.createNewPassword,
         name: AppRoutes.createNewPassword,
         builder: (context, state) => const CreateNewPassword(),
+      ),
+      GoRoute(
+        path: AppRoutes.statistics,
+        name: AppRoutes.statistics,
+        builder: (context, state) => const StatisticsScreen(),
       ),
     ],
   );
