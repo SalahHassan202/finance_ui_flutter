cd2fd953686eb6ef058942f87103ac47c078507c
SalahHassan202 <salahhassan66190@gmail.com>
2025-11-21 21:43:53 +0200

    Update button routing

--- lib/features/forget_password/forget_password.dart
+++ lib/features/forget_password/forget_password.dart
@@ -1,42 +1,64 @@
+import 'package:finance_ui_flutter/core/routing/app_route.dart';
+import 'package:finance_ui_flutter/core/styling/app_colors.dart';
 import 'package:finance_ui_flutter/core/styling/app_styles.dart';
 import 'package:finance_ui_flutter/core/widgets/back_button_widget.dart';
 import 'package:finance_ui_flutter/core/widgets/custom_text_field.dart';
+import 'package:finance_ui_flutter/core/widgets/primary_button_widget.dart';
 import 'package:finance_ui_flutter/core/widgets/spacing_widgets.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class ForgetPassword extends StatelessWidget {
   const ForgetPassword({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Column(
-        children: [
-          const HeightSpace(12),
-          BackButtonWidget(),
-          const HeightSpace(28),
-          Text("Forgot Password?", style: AppStyles.primaryHeadLineStyle),
-          const HeightSpace(10),
-          Text(
-            "Don't worry! It occurs. Please enter the email address linked with your account.",
-            style: AppStyles.subtitlesStyles,
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 22.w),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const HeightSpace(12),
+              const BackButtonWidget(),
+              const HeightSpace(28),
+              Text("Forgot Password?", style: AppStyles.primaryHeadLineStyle),
+              const HeightSpace(10),
+              Text(
+                "Don't worry! It occurs. Please enter the email address linked with your account.",
+                style: AppStyles.subtitlesStyles,
+              ),
+              const HeightSpace(32),
+              const CustomTextField(hintText: "Enter Your email"),
+              const HeightSpace(38),
+              PrimayButtonWidget(
+                buttonText: "Send Code",
+                onPress: () {
+                  GoRouter.of(context).pushNamed(AppRoutes.verifyOtpScreen);
+                },
+              ),
+              const HeightSpace(361),
+              Center(
+                child: RichText(
+                  text: TextSpan(
+                    text: "Remember Password? ",
+                    style: AppStyles.subtitlesStyles,
+                    children: [
+                      TextSpan(
+                        text: "Login",
+                        style: AppStyles.black15BoldStyle.copyWith(
+                          color: AppColors.primaryColor,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
           ),
-          const HeightSpace(32),
-          CustomTextField(hintText: "Enter Your email"),
-          const HeightSpace(38),
-          CustomTextField(hintText: "Send Code"),
-          const HeightSpace(361),
-          RichText(
-            text: TextSpan(
-              text: "Remember Password?",
-              style: AppStyles.primaryHeadLineStyle,
-
-              children: [
-                TextSpan(text: "LOgin", style: AppStyles.black15BoldStyle),
-              ],
-            ),
-          ),
-        ],
+        ),
       ),
     );
   }
