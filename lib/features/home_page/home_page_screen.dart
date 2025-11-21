7b13ff96b5ae63def2ca4618b94da14bd57b1dbf
SalahHassan202 <salahhassan66190@gmail.com>
2025-11-21 21:45:35 +0200

    Refine GridView layout

--- lib/features/home_page/home_page_screen.dart
+++ lib/features/home_page/home_page_screen.dart
@@ -5,57 +5,58 @@ import 'package:finance_ui_flutter/features/home_page/widgets/top_profile_notifi
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-class HomePageScreen extends StatefulWidget {
+class HomePageScreen extends StatelessWidget {
   const HomePageScreen({super.key});
 
   @override
-  State<HomePageScreen> createState() => _HomePageScreenState();
-}
-
-class _HomePageScreenState extends State<HomePageScreen> {
-  @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.symmetric(horizontal: 24.w),
-
-      child: Column(
-        children: [
-          HeightSpace(18),
-          TopProfileNotificationWidget(),
-          HeightSpace(18),
-          CustomCarsouselWidget(),
-          HeightSpace(24),
-
-          GridView(
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2,
-              mainAxisSpacing: 16.sp,
-              crossAxisSpacing: 16.sp,
+    return SingleChildScrollView(
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 24.w),
+        child: Column(
+          children: [
+            const HeightSpace(18),
+            const TopProfileNotificationWidget(),
+            const HeightSpace(18),
+            const CustomCarsouselWidget(),
+            const HeightSpace(24),
+            SizedBox(
+              height: 400.h,
+              child: GridView(
+                physics: const NeverScrollableScrollPhysics(),
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  mainAxisSpacing: 16.sp,
+                  crossAxisSpacing: 16.sp,
+                  childAspectRatio: 0.85,
+                ),
+                children: const [
+                  CustomHomePageItem(
+                    iconData: Icons.send,
+                    title: "Send Money",
+                    description: "Take acc to acc",
+                  ),
+                  CustomHomePageItem(
+                    iconData: Icons.send,
+                    title: "Pay the bill",
+                    description: "Pay Money",
+                  ),
+                  CustomHomePageItem(
+                    iconData: Icons.send,
+                    title: "Request",
+                    description: "Send a Reqest",
+                  ),
+                  CustomHomePageItem(
+                    iconData: Icons.send,
+                    title: "Contact",
+                    description: "Contact to Other",
+                  ),
+                ],
+              ),
             ),
-            children: [
-              CustomHomePageItem(
-                iconData: Icons.send,
-                title: "Send Money",
-                description: "Take acc to acc",
-              ),
-              CustomHomePageItem(
-                iconData: Icons.send,
-                title: "Pay the bill",
-                description: "Pay Money",
-              ),
-              CustomHomePageItem(
-                iconData: Icons.send,
-                title: "Request",
-                description: "Send a Reqest",
-              ),
-              CustomHomePageItem(
-                iconData: Icons.send,
-                title: "Contact",
-                description: "Contact to Other",
-              ),
-            ],
-          ),
-        ],
+            const HeightSpace(24),
+          ],
+        ),
       ),
     );
   }
