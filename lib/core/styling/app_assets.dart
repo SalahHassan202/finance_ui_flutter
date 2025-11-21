28c8a0b72b3a5dcac2e48da81e4db426625d4249
SalahHassan202 <salahhassan66190@gmail.com>
2025-11-21 16:01:06 +0200

    organize  app assets

--- lib/core/styling/app_assets.dart
+++ lib/core/styling/app_assets.dart
@@ -3,6 +3,9 @@ class AppAssets {
   static const String profile = "assets/images/profile.jpeg";
   static const String layer1 = "assets/images/layer1.png";
   static const String layer2 = "assets/images/layer2.png";
+  static const String successmark = "assets/images/successmark.png";
+  static const String card0 = "assets/images/card0.png";
+  static const String card1 = "assets/images/card1.png";
 
   static const String facebookIcon = "assets/images/facebook.svg";
   static const String googleIcon = "assets/icons/google.svg";
